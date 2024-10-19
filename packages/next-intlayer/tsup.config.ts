bf33f09d6dcac92b9d5e09a39712b0629475b1d3
Aymeric PINEAU <ay.pineau@gmail.com>
2024-10-19 16:55:06 -0600

    feat(core): switch package builder to vite lib

--- packages/next-intlayer/tsup.config.ts
+++ packages/next-intlayer/tsup.config.ts
@@ -1,6 +1,9 @@
 import { packageBuildOptions } from '@utils/ts-config/tsup';
 import { type Options, defineConfig } from 'tsup';
 
-const option: Options[] = packageBuildOptions as Options[];
+const option: Options[] = (packageBuildOptions as Options[]).map((option) => ({
+  ...option,
+  outDir: 'dist',
+}));
 
 export default defineConfig(option);
